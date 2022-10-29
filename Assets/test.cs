b3b600f55a43b52ca08fa8471851dce35a8986dc
シュール <shule517@gmail.com>
2022-10-30 07:51:09 +0900

    Macから追加

--- Assets/test.cs
+++ Assets/test.cs
@@ -7,7 +7,7 @@ public class test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Macからテストで追加しました
     }
 
     // Update is called once per frame
